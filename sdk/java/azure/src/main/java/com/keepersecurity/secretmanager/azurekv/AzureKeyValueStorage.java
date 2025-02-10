96a5fbe9e30872d75dfc4a941296e393067ba33d
Sajid Ali <sajid.ali@metronlabs.com>
2025-02-10 18:49:35 +0530

    Added copyright

--- sdk/java/azure/src/main/java/com/keepersecurity/secretmanager/azurekv/AzureKeyValueStorage.java
+++ sdk/java/azure/src/main/java/com/keepersecurity/secretmanager/azurekv/AzureKeyValueStorage.java
@@ -37,6 +37,19 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+/**
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ (R)
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Secrets Manager
+# Copyright 2025 Keeper Security Inc.
+# Contact: sm@keepersecurity.com
+**/
+
+
 public class AzureKeyValueStorage implements KeyValueStorage{
 
 	private String defaultConfigFileLocation = "client-config.json";
