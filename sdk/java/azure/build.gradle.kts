e409487322870993f85a7eca124542f0b7cf2b23
Sajid Ali <sajid.ali@metronlabs.com>
2025-02-13 17:06:55 +0530

    Updated build.gradle

--- sdk/java/azure/build.gradle.kts
+++ sdk/java/azure/build.gradle.kts
@@ -1,5 +1,16 @@
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.signing
+
+group = "com.keepersecurity.secrets-manager.azure"
+version = "1.0.0"
+
 plugins {
     id ("java");
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 java {
@@ -17,6 +28,9 @@ dependencies {
     implementation("com.azure:azure-identity:1.15.0")
     implementation("com.azure:azure-security-keyvault-keys:4.9.2")
     implementation("com.google.code.gson:gson:2.12.1")
+    
+    
+    testImplementation("org.bouncycastle:bc-fips:2.0.0")
 }
 
 
