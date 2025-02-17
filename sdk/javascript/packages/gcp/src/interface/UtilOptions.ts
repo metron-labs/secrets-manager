3015aa9b9dcae23b4cb9ee5570e06c64d5e2b17f
satish-metron <satish.gaddala@metronlabs.com>
2025-02-17 19:35:50 +0530

    KEEP-140-added GCP support to SDK

---
+++ sdk/javascript/packages/gcp/src/interface/UtilOptions.ts
@@ -0,0 +1,30 @@
+import { KeyManagementServiceClient } from '@google-cloud/kms';
+import { GCPKeyConfig } from 'src/GcpKeyConfig';
+
+export type KMSClient = InstanceType <typeof KeyManagementServiceClient> ;
+
+export interface Options{
+  isAsymmetric: boolean;
+  cryptoClient: KMSClient;
+  keyProperties : GCPKeyConfig;
+};
+
+export interface BufferOptions extends Options{
+  keyType: string;
+};
+
+export interface EncryptBufferOptions extends BufferOptions{
+  message: string;
+};
+
+export interface DecryptBufferOptions extends BufferOptions{
+  ciphertext: Buffer;
+};
+
+export interface encryptOptions extends Options {
+  message: Buffer;
+};
+
+export interface decryptOptions extends Options {
+  cipherText: Buffer;
+};
